8b9d568c2e6f7b6dfbb838ce4eb1b7eda671771f
Luis Fernando Ttito Surco <luis.ttito@ucsp.edu.pe>
2019-06-25 04:29:00 -0500

    v2

---
+++ Escape-Game/Assets/BicycleSimulator/Scripts/PlayerMotor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private float velocity = 0;
+    private Vector3 rotation = Vector3.zero;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(float _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformMovement()
+    {
+        /*if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+        */
+        rb.velocity = rb.transform.forward * velocity;
+    }
+
+    void PerformRotation()
+    {
+        rb.transform.Rotate(rotation); 
+    }
+}
